ecbd8f50b144164d24ba3b084a586117e5fb3982
5d-jh <jeonghyun0126@outlook.com>
2019-01-03 22:09:22 +0900

    Initial commit

---
+++ neis_code_finder/get_code.py
@@ -0,0 +1,35 @@
+import re
+import requests
+
+def get(school):
+    uri = 'https://www.schoolinfo.go.kr/ei/ss/Pneiss_f01_l0.do'
+    form = {
+        "SEARCH_GS_HANGMOK_CD": "",
+        "SEARCH_GS_HANGMOK_NM": "",
+        "SEARCH_SCHUL_NM": school.encode('EUC-KR'),
+        "SEARCH_GS_BURYU_CD": "",
+        "SEARCH_KEYWORD": school.encode('EUC-KR')
+    }
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+
+    r = requests.post(uri, data=form, headers=headers)
+
+    # print(r.text)
+
+    results_code = re.findall(r'searchSchul\(\'([A-Za-z0-9_]+)\'\)', r.text)
+    results_code = results_code[::2]
+
+    results_address = re.findall(r'학교주소<\/span>(.*)<\/li>', r.text)
+    results_address.pop()
+
+    iters_num = len(results_code)
+    results = []
+    for i in range(iters_num):
+        results.append({
+            'code': results_code[i],
+            'address': results_address[i]
+        })
+        
+    return results
