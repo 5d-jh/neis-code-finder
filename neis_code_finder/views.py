68baaa7e655169bdbfcfd4a8255972083a9e1a22
5d-jh <jeonghyun0126@outlook.com>
2019-01-16 00:25:10 +0900

    query 오류 수정

--- neis_code_finder/views.py
+++ neis_code_finder/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from . import get_code
 
 def get_page(req):
-    template_data = {
-        'school_infos': get_code.get(req.GET['q']),
-        'query': req.GET['q']
-    }
+    template_data = {}
+    if 'q' in req.GET:
+        template_data['school_infos'] = get_code.get(req.GET['q'])
+        template_data['query'] = req.GET['q']
+
     return render(req, 'neis_code_finder/main.html', template_data)
