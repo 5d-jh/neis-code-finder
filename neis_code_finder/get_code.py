c102752b8f9d944685257955ae6127eb72b1ee02
5d-jh <jeonghyun0126@outlook.com>
2019-02-05 15:40:07 +0900

    학교이름, 학교유형 지원

--- neis_code_finder/get_code.py
+++ neis_code_finder/get_code.py
@@ -1,5 +1,6 @@
 import re
 import requests
+from bs4 import BeautifulSoup
 
 def get(school):
     uri = 'https://www.schoolinfo.go.kr/ei/ss/Pneiss_f01_l0.do'
@@ -16,21 +17,34 @@ def get(school):
 
     r = requests.post(uri, data=form, headers=headers)
 
-    # print(r.text)
+    soup = BeautifulSoup(r.text)
+    school_infos = soup.find_all(class_="SchoolList")
 
-    results_code = re.findall(r'searchSchul\(\'([A-Za-z0-9_]+)\'\)', r.text)
-    results_code = results_code[::2]
+    reg_code = r'searchSchul\(\'([A-Za-z0-9_]+)\'\)'
+    results_code = [re.findall(reg_code, str(info.a))[0] for info in school_infos]
 
-    results_address = re.findall(r'학교주소<\/span>(.*)<\/li>', r.text)
-    if len(results_address) != 0:
-        results_address.pop()
+    results_name = [info.a.string for info in school_infos]
+    
+    reg_addr = r'<\/span>(.*)<\/li>'
+    results_address = [str(info.find_all('li')[1]) for info in school_infos]
+    results_address = [re.findall(reg_addr, addr)[0] for addr in results_address]
+
+    school_types = {
+        '초': 'elementary',
+        '중': 'middle',
+        '고': 'high'
+    }
+    results_type = [info.find_all(class_="mapD_Class")[0].string for info in school_infos]
+    results_type = [school_types[sch_type] for sch_type in results_type]
 
     iters_num = len(results_code)
     results = []
     for i in range(iters_num):
         results.append({
             'code': results_code[i],
-            'address': results_address[i]
+            'address': results_address[i],
+            'name': results_name[i],
+            'type': results_type[i]
         })
-        
+
     return results
